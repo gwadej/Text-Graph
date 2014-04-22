f1832a9f20232973efbabc0500960e5e47685226
G. Wade Johnson <gwadej@anomaly.org>
2014-04-22 12:21:26 -0500

    Remove Exporter dependency.

--- lib/Text/Graph/DataSet.pm
+++ lib/Text/Graph/DataSet.pm
@@ -2,18 +2,8 @@ package Text::Graph::DataSet;
 
 use strict;
 use warnings;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
-require Exporter;
-
-@ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-@EXPORT = qw();
-
-$VERSION = '0.21';
+our $VERSION = '0.21';
 
 sub new
 {
